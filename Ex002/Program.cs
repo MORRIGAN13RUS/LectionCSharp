8692c49297fa9231e1ee107184637c0615a14f3f
Morrigan <stepanyancarine@yandex.ru>
2022-08-21 07:56:09 +0300

    Функции создания массива, выведения его и поиска элемента в нем

---
+++ Ex002/Program.cs
@@ -0,0 +1,30 @@
+﻿void FiillArray(int[] collection){
+    int i=0;
+    while(i<collection.Length){
+        collection[i]=new Random().Next(1,100);
+        i++;
+    }
+}
+void PrintArray(int[]col){
+    int i=0;
+    while(i<col.Length){
+        Console.WriteLine(col[i]);
+        i++;
+    }
+}
+int Indexof(int[] collection, int find){
+    int i=0;
+    int position=-1;
+    while(i<collection.Length){
+        if(collection[i]==find) position=i;
+        break;
+    i++;
+}
+return position;
+}
+int[]array=new int[10];
+FiillArray(array);
+PrintArray(array);
+Console.WriteLine();
+int pos=Indexof(array,4);
+Console.WriteLine(pos);
